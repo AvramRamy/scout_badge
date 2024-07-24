e7d5d5ca7f6580ebea49160d02aee9898238fdac
Avram Ramy <142695051+AvramRamy@users.noreply.github.com>
2024-07-25 01:32:30 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+// const number = document.getElementById('number').value;
+// const place = document.getElementById('place');
+// const x = number * 250
+// place.innerHTML = x
+function avram() {
+    const number = document.getElementById('number').value;
+    const place = document.getElementById('place');
+    const x = number * 250
+    place.innerHTML = x
+}
